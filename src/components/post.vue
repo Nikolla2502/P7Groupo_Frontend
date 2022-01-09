1ac1ead37c1b3e5e7e3faedfa7b8a58f2aa92e52
Nikolla2502 <nikolla@wanadoo.fr>
2022-01-09 11:15:42 +0700

    ajout deconnexion_profil

--- src/components/post.vue
+++ src/components/post.vue
@@ -1,7 +1,13 @@
 <template>
 <div>
      <Banner message="Votre réseau d'entreprise">
-         </Banner>
+
+     </Banner>
+    <ul>
+        <li class='userProfil'>Profil</li>
+        <li>Deconnexion</li>
+    </ul>
+    <div class="contenu"></div>
     <Footer></Footer>
 
 </div>
@@ -23,6 +29,23 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+ul {
+    position: absolute;
+    top:30px;
+    right:30px;
+    background: #FFF;
+    margin-top: 5px;
+    text-align: left;
+    padding:5px 10px;
+    display: inline-block;
+    list-style: none;
+    border-radius: 5px;
+    border:solid 1px #fd2d01;
+    box-shadow: 2px 2px 2px 0px rgba(0,0,0,0.75);
+}
+.contenu {
+    height: 1500px;
+}
 
 </style>
